52efa09b595d5b39974383ec075900a119ffc5d5
Liza Shulika <liza.shulika2009@gmail.com>
2025-04-08 20:55:36 +0300

    taxi

--- taxi/views.py
+++ taxi/views.py
@@ -1,4 +1,7 @@
+from django.http import Http404, HttpResponse
 from django.shortcuts import render
+from django.views import generic
+from django.views.generic import ListView
 
 from taxi.models import Driver, Car, Manufacturer
 
@@ -13,3 +16,41 @@ def index(request):
     }
 
     return render(request, "taxi/index.html", context=context)
+
+
+class ManufacturerListView(generic.ListView):
+    model = Manufacturer
+    queryset = Manufacturer.objects.all().order_by("name")
+    paginate_by = 5
+
+
+class CarListView(generic.ListView):
+    model = Car
+    queryset = Car.objects.select_related("manufacturer").all()
+    paginate_by = 5
+
+
+class CarDetailView(generic.DetailView):
+    model = Car
+
+
+class DriverListView(generic.ListView):
+    model = Driver
+    paginate_by = 5
+
+
+class DriverDetailView(generic.DetailView):
+    model = Driver
+    queryset = Driver.objects.all()
+
+
+def driver_detail_view(request, HttpRequest, pk: int) -> HttpResponse:
+    try:
+        driver = Driver.objects.get(pk=pk)
+    except Driver.DoesNotExist:
+        raise Http404
+    context = {
+        "driver": driver,
+    }
+    return render(request, "taxi/driver_detail.html", context=context)
+
